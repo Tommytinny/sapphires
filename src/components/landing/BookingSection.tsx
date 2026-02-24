c95f5268b708763fa8a2986df56bd55c6fea55ee
Tommy <tommyteenboi@gmail.com>
2026-02-24 22:54:40 +0100

    add new update

--- src/components/landing/BookingSection.tsx
+++ src/components/landing/BookingSection.tsx
@@ -63,6 +63,7 @@ const BookingSection = () => {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      'Authorization': `Bearer ${import.meta.env.VITE_SUPABASE_ANON_KEY}`
     },
     body: JSON.stringify({
       projectName: formData.projectName,
